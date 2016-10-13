ad8d5db10de0075d88c2ea1af74570f3090bde2c
Scott Heinrich <scottaheinrich@gmail.com>
2016-10-12 22:45:27 -0600

    Rotating images!

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,25 @@
-// 1. Put an on click listen event on each of the buttons
-//
-// 2. On click it will print out on the console it's name or internal text using a javascript closure
-// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
-
-
-
-document.getElementById("first").onclick = function() {getText('first')};
-document.getElementById("second").onclick = function() {getText('second')};
-document.getElementById("third").onclick = function() {getText('third')};
-
-
-function getText(button)
-{
-  var text = document.getElementById(button).textContent;
-  function logPrint ()
-  {
-    console.log(text);
+document.getElementById("confirmButton").onclick = function(){
+  if(!validateEmail(document.getElementById("email").value)){
+    alert("Please enter a valid email!");
   }
-  logPrint();
+  else if (document.getElementById("email").value != document.getElementById("email2").value) {
+    alert("Email addresses do not match!");
+  }
+  else if (!document.getElementById("firstName").value){
+    alert("Please enter a first name!");
+  }
+  else if (!document.getElementById("lastName").value){
+    alert("Please enter a last name!");
+  }
+  else if (document.getElementById("firstName").value == "Donald" && document.getElementById("lastName").value == "Trump"){
+    alert("You and your ENTIRE campaign team each receive a free case of Kool-Aid! Drink up!");
+  }
+  else{
+    alert("Your order has been placed!");
+  }
+}
 
-};
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
